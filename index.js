c60a69b6bac706e0672090451d5f2f027ca29242
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 20:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var myArray = []
+  for( let i=0; i<musicians.length; i++ ){
+    let string = `${musicians[i]} plays ${instruments[i]}`
+    myArray.push(string)
+  }
+  return myArray
+}
+
+function johnLennonFacts(facts){
+  let myArray = []
+  let i = 0
+  while( i < facts.length ){
+    let string = facts[i] + '!!!'
+    myArray.push(string)
+    i++
+  }
+  
+  return myArray
+}
+
+function iLoveTheBeatles(num){
+  var myArray = []
+  do{
+    
+  }while()
+}
